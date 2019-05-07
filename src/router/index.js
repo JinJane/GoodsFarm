b524bc0744e83f5e8d8743174aefeb148e694376
JinJane <j.songprakob@gmail.com>
2019-05-07 16:50:18 +0700

    web

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Sell from '@/components/Sell'
+import Profile from '@/components/Profile'
+import Order from '@/components/Order'
+import GoodsDetail from '@/components/GoodsDetail'
+import Bucket from '@/components/Bucket'
+import AddGoods from '@/components/AddGoods'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/sell',
+      name: 'Sell',
+      component: Sell
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/order',
+      name: 'Order',
+      component: Order
+    },
+    {
+      path: '/goodsdetail',
+      name: 'GoodsDetail',
+      component: GoodsDetail
+    },
+    {
+      path: '/bucket',
+      name: 'Bucket',
+      component: Bucket
+    },
+    {
+      path: '/addgoods',
+      name: 'AddGoods',
+      component: AddGoods
+    },
+  ]
+})
