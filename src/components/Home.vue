a2f3a134ccf5f753e7336a628a39693b9a2c0861
egco427_assignment1_5913218 <sawanya000@gmail.com>
2019-05-07 21:58:49 +0700

    Bar

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <BarBeforeLogin/>
+    </div>
+</template>
+<script>
+import BarBeforeLogin from './elements/BarBeforeLogin'
+export default {
+    components:{BarBeforeLogin}
+}
+</script>
