1882cbd0aa544f1aeae92fdc8f9ccbe30c7f6baf
Unknown <sawanya000@gmail.com>
2019-05-09 22:16:40 +0700

    upp

--- src/components/Bucket.vue
+++ src/components/Bucket.vue
@@ -8,4 +8,23 @@
     <br>
     <img src="https://goodsfarm-backend-garking.c9users.io/getpicture" alt="">
     </div>
+    
 </template>
+<script>
+
+export default {
+   methods:{
+       mmm(){
+           console.log("mm")
+           console.log(document.getElementById('filename').ev)
+    //        document.getElementById('filename').value = uploadOnChange;
+    //        var filename = this.value;
+    // var lastIndex = filename.lastIndexOf("\\");
+    // if (lastIndex >= 0) {
+    //     filename = filename.substring(lastIndex + 1);
+    // }
+    // document.getElementById('filename').value = filename;
+       }
+   }
+}
+</script>
